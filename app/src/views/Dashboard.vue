349a43eb4e7f64cf55710aaf8e9268e92fe2fc7d
lstubdal <lotte.stubdal@gmail.com>
2023-01-25 11:43:39 +0000

    Add view for logged in an not

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -1,18 +1,47 @@
 <template>
-   <div class="dashboard">
-      <h1>LESEHESTENE</h1>
+   <div v-if="user.loggedIn" class="dashboard--loggedIn">
+      <p>What have others been reading?</p>
+   </div>
+
+   <div v-else class="dashboard">
+
    </div>
 </template>
  
 <script>
+   import { getAuth } from 'firebase/auth';
+
    export default {
-   }
+        created() {
+            // get the current user by setting an observer on the Auth object:
+            const auth = getAuth()
+            auth.onAuthStateChanged(user => {
+                this.$store.dispatch('fetchUser', user)
+            })
+        },
+
+        computed: {
+            user() {
+                return this.$store.getters.getUser
+            },
+        }
+    }
 </script>
 <style>
    .dashboard {
+      height: 100%;
       display: flex;
       justify-content: center;
       align-items: center;
+      background-image: url("/images/background.jpg");
+      background-size: cover;
+   }
+
+   .dashboard--loggedIn {
+      font-family: var(--second-font);
+      font-weight: 400;
+      font-size: 2.4em;
+      padding: 100px;
    }
 </style>
  
