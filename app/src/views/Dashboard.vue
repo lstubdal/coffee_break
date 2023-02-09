981e30661b5362219ee52bbe2801f49fd30016f7
lstubdal <lotte.stubdal@gmail.com>
2023-02-09 23:16:26 +0000

    Fetch all recommendations

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -1,6 +1,16 @@
 <template>
    <div v-if="user.loggedIn" class="dashboard--loggedIn">
-      <p>What have others been reading?</p>
+      <p class="dashboard__heading">What have others been reading?</p>
+      <div class="books" v-if="!loading" v-for="book in this.result">
+         <Book 
+            :recommender="book.recommender"
+            :title="book.title"
+            :author="book.author"
+            :description="book.description"
+            :category="book.category"
+            :rating="book.rating"
+         />
+      </div>
    </div>
 
    <div v-else class="dashboard">
@@ -10,20 +20,30 @@
  
 <script>
    import { getAuth } from 'firebase/auth';
+   import sanityMixin from '../mixins/sanityMixin';
+   import allBooks from '../groq/allBooks.groq?raw';
+   import Book from '../components/Book.vue';
 
    export default {
-        created() {
-            // get the current user by setting an observer on the Auth object:
-            const auth = getAuth()
-            auth.onAuthStateChanged(user => {
-                this.$store.dispatch('fetchUser', user)
-            })
+      mixins: [sanityMixin],
+      async created() {
+         // get the current user by setting an observer on the Auth object:
+         const auth = getAuth()
+         auth.onAuthStateChanged(user => {
+               this.$store.dispatch('fetchUser', user)
+         })
+
+         await this.sanityFetch(allBooks);
         },
 
         computed: {
             user() {
                 return this.$store.getters.getUser
             },
+        }, 
+        
+        components: {
+         Book
         }
     }
 </script>
@@ -38,10 +58,24 @@
    }
 
    .dashboard--loggedIn {
+      height: 100%;
+      padding: 80px;
+      background-color: var(--background);
+      color: var(--text)
+     
+   }
+
+   .dashboard__heading {
       font-family: var(--second-font);
       font-weight: 400;
       font-size: 2.4em;
-      padding: 100px;
    }
+
+/*    .books {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-auto-rows: auto;
+      border: 5px solid red;
+   } */
 </style>
  
