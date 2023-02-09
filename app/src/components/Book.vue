0be39541a11bc6c209d2cb5067cc95aa7b38edfa
lstubdal <lotte.stubdal@gmail.com>
2023-02-09 23:14:52 +0000

    Create book component with props

---
+++ app/src/components/Book.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="book">
+        <p>Recommended by: {{ recommender }}</p>
+        <h3>{{ title }}</h3>
+        <p>by: {{ author }}</p>
+        <p>{{ description }}</p>
+        <p>{{ rating }}</p>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            recommender: { type: String },
+            title: { type: String },
+            author: { type: String },
+            description: {type: String},
+            rating: { type: String }
+        }
+    }
+</script>
+<style>
+    .book {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
